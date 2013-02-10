5372f563c530ddb2cccf08dcc78d9ae987627fa1
Brian Smith <brian@dialexa.com>
2013-02-10 00:31:28 -0800

    proxy set methods

--- lib/polyamorous/associations/collection_proxy.rb
+++ lib/polyamorous/associations/collection_proxy.rb
@@ -4,10 +4,11 @@ module Polyamorous
       def initialize(ass_class, klass)
         @ass_class = ass_class
         @klass = klass
+        @collection = []
       end
       
       def set(collection)
-        
+        @collection = collection
       end
     end
   end
--- lib/polyamorous/associations/singular_proxy.rb
+++ lib/polyamorous/associations/singular_proxy.rb
@@ -4,10 +4,11 @@ module Polyamorous
       def initialize(ass_class, klass)
         @ass_class = ass_class
         @klass = klass
+        @instance = nil
       end
       
       def set(instance)
-        
+        @instance = instance
       end
     end
   end
